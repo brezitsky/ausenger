12d27e8d02c8ed8a86bb0b1cfa3c7a1ce2d79047
MrWHvb <vladikbrezitsky@gmail.com>
2018-02-23 15:32:14 +0200

    work

--- bots/telegram/index.js
+++ bots/telegram/index.js
@@ -1,22 +1,49 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '536012378:AAGyTAsny5Llqg4SF5--iICStKmlb-d0IrQ';
 
-const bot = new TelegramBot(token, {polling: true});
+console.log('Bot has been started...');
+const bot = new TelegramBot(token, {
+
+	polling: {
+		interval: 300,
+		autoStart: true,
+		params: {
+			timeout: 10
+		}
+	}
+
+});
 
 bot.onText(/^\/keyboard$/g, (msg) => {
-	const chatId = msg.chat.id;
+	const chatId = msg.chat.id
 
 	var keyboard = {
 		reply_markup: JSON.stringify({
 			keyboard: [
-				['Uno'],
-				['Dos'],
-				['Tres'],
-				['Cuatro']
+				['один'],
+				['два'],
+				['три'],
+				['чотири']
 			]
 		})
-	};
+	}
 
 	// send a message to the chat acknowledging receipt of their message
 	bot.sendMessage(chatId, 'hello world', keyboard);
+});
+bot.on('message', (msg) => {
+	console.log(msg);
+	if(msg.text == 'один') {
+		bot.sendMessage(msg.chat.id, 'Ви ввели ' + msg.text + ', ' + msg.from.first_name);
+	}
+	else if(msg.text == 'два') {
+		bot.sendMessage(msg.chat.id, 'Ви ввели ' + msg.text + ', ' + msg.from.first_name);
+	}
+	else if(msg.text == 'три') {
+		bot.sendMessage(msg.chat.id, 'Ви ввели ' + msg.text + ', ' + msg.from.first_name);
+	}
+	else if(msg.text == 'чотири') {
+		bot.sendMessage(msg.chat.id, 'Ви ввели ' + msg.text + ', ' + msg.from.first_name);
+	}
+	else bot.sendMessage(msg.chat.id, 'Ви ввели якусь дічь, ' + msg.from.first_name);
 });
